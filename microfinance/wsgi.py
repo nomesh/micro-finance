11b90e96bf2a5d1b154d04f69530ac8b24ea10a4
nomesh <nomesh@users.noreply.github.com>
2026-02-24 17:32:06 +0530

    Fix WSGI configuration

--- microfinance/wsgi.py
+++ microfinance/wsgi.py
@@ -1,18 +1,14 @@
 import os
 import sys
 
+# Add project to path
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE_DIR)
+
 # Apply Django 1.11 Python 3.8+ compatibility patch
 import django_patch
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "microfinance.settings")
+
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
-
-PROJECT_DIR = os.path.abspath(__file__)
-sys.path.append(PROJECT_DIR)
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "microfinance.settings_local")
-
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
