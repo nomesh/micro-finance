93a67e3ed68876cf99fc1282fc256726dc6f0053
nomesh <nomesh@users.noreply.github.com>
2026-02-24 17:07:40 +0530

    Pin pip version for old package compatibility

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
+pip install --upgrade pip==21.3.1
 pip install -r requirements.txt
 python manage_local.py collectstatic --no-input
 python manage_local.py migrate
