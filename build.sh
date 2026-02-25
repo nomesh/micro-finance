c0c405eab13a3a85e0ffe00b32f437bc4915e337
nomesh <nomesh@users.noreply.github.com>
2026-02-25 18:12:03 +0530

    Add load_defaults command to populate loan repayment frequency dropdown

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ pip install --upgrade pip==21.3.1
 pip install -r requirements.txt
 python manage_local.py collectstatic --no-input
 python manage_local.py migrate
+python manage_local.py load_defaults
